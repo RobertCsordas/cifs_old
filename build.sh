3957c2eac1376a39e17b7d047d4adcf132cbc0a5
Robert Csordas <robert.csordas@aimotive.com>
2018-01-12 10:02:40 +0100

    Should work with ubuntu

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 make clean
 make
-cp cifs_old.ko /lib/modules/extramodules-$(uname -r|sed 's/\./ /g'|awk '{print $1"."$2}')-ARCH/
+if [[ `uname -r|grep ARCH` == "" ]]; then
+	cp cifs_old.ko /lib/modules/extramodules-$(uname -r|sed 's/\./ /g'|awk '{print $1"."$2}')-ARCH/
+else
+	cp cifs_old.ko /lib/modules/$(uname -r)/kernel/fs
+fi
 depmod
 echo "blacklist cifs" > /etc/modprobe.d/nocifs.conf
 echo "cifs_old" > /etc/modules-load.d/cifs_old.conf
